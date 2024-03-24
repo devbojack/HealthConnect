7d396e4f5b0e1a4f1efcd7d6f12e94ea1fe87753
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-03-25 00:58:58 +0300

    Complete Google Auth UI

--- lib/screens/authentication_screens/google_auth/google_auth_page.dart
+++ lib/screens/authentication_screens/google_auth/google_auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:healthconnect/widgets/background.dart';
 
 import '../../../constants/assets_constants.dart';
@@ -14,24 +15,52 @@ class _GoogleAuthPageState extends State<GoogleAuthPage> {
   @override
   Widget build(BuildContext context) {
     return MyBackground(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(AssetConstants.lightLogoImage), fit: BoxFit.contain,
-                )
-              )
-            ),
-            const SizedBox(height: 32),
-            CircularProgressIndicator(color: Theme.of(context).indicatorColor),
-          ]
-        )
-      )
+      child: Scaffold(
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          Theme.of(context).brightness == Brightness.dark
+                              ? AssetConstants.lightAppLogo
+                              : AssetConstants.darkAppLogo),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                SizedBox(
+                  height: 32,
+                  width: 32,
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).indicatorColor),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Container(height: 24, width: 24, decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(AssetConstants.googleLogo), fit: BoxFit.contain,
+                    )
+                  )),
+                  const SizedBox(width: 12),
+                  Text(
+                  'Loading...',
+                  style: GoogleFonts.roboto(
+                    fontSize: 12,
+                    color: Theme.of(context).indicatorColor.withOpacity(0.6),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),])
+              ]),
+        ),
+      ),
     );
   }
 }

d13c4fae80cd8309ce298b5f5353445626853d51
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-03-28 15:35:55 +0300

    Update home appbar - With a row and a spacer

--- lib/widgets/home_appbar.dart
+++ lib/widgets/home_appbar.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:healthconnect/constants/assets_constants.dart';
 import 'package:healthconnect/identifications/secret_ids.dart';
-import 'package:healthconnect/widgets/text_icon_widgets.dart';
 
-AppBar homeAppBar(String title, BuildContext context) {
+AppBar homeAppBar(BuildContext context) {
   return AppBar(
     automaticallyImplyLeading: false,
     elevation: 0,
@@ -33,27 +32,33 @@ AppBar homeAppBar(String title, BuildContext context) {
                     style: GoogleFonts.roboto(
                       fontSize: 11,
                       fontWeight: FontWeight.w700,
-                      color: Theme.of(context).indicatorColor.withOpacity(0.7),
+                      color: Theme.of(context).dividerColor.withOpacity(0.7),
                     ),
                   ),
                   Text(UserData.userName,
                       style: GoogleFonts.roboto(
                         fontSize: 12,
                         fontWeight: FontWeight.bold,
-                        color:
-                            Theme.of(context).indicatorColor.withOpacity(0.8),
+                        color: Theme.of(context).dividerColor.withOpacity(0.8),
                       )),
                 ]),
             const SizedBox(width: 6),
-            Container(
-              height: 32,
-              width: 32,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  image: const DecorationImage(
-                    image: AssetImage(UserData.userPic),
-                    fit: BoxFit.contain,
-                  )),
+            Column(
+              children: [
+                Container(
+                  height: 32,
+                  width: 32,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    image: const DecorationImage(
+                      image: AssetImage(UserData.userPic),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Text('HealthConnect',
+                    style: Theme.of(context).textTheme.bodyMedium),
+              ],
             )
           ]),
         )

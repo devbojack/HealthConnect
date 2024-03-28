b08de687c498256e956d22643f02892055618b83
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-03-28 15:34:37 +0300

    Fix internet page - Overflow issue

--- lib/pages/options/no_internet_page.dart
+++ lib/pages/options/no_internet_page.dart
@@ -29,13 +29,12 @@ class _NoInternetPageState extends State<NoInternetPage> {
       child: SafeArea(
         child: Scaffold(
           body: isLoading
-              ? const Center(
-                  child: SbLoading()
-                )
+              ? const Center(child: SbLoading())
               : Container(
                   width: double.infinity,
                   height: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                   child: Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,18 +47,23 @@ class _NoInternetPageState extends State<NoInternetPage> {
                             Lottie.asset(noInternetLottie, fit: BoxFit.contain),
                       ),
                       Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          veryBigTextBold('No Internet Connection', mHeight, context),
+                          veryBigTextBold(
+                              'No Internet Connection', mHeight, context),
                           const SizedBox(height: 8),
                           SizedBox(
                               width: mWidth * 20,
                               child: Divider(
-                                color: Theme.of(context).dividerColor,
-                                thickness: 1
-                              )),
+                                  color: Theme.of(context).dividerColor,
+                                  thickness: 1)),
                           const SizedBox(height: 32),
                           SizedBox(
-                            child: normalText('Please check your Wi-Fi or mobile data network and try again.', context),
+                            child: Text(
+                                'Please check your Wi-Fi or mobile data network and try again.',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                textAlign: TextAlign.center),
                           ),
                         ],
                       ),

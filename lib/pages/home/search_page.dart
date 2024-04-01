bfc5abd3dc63f6e72e96eb0881e7e46eddf97250
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-04-01 10:14:48 +0300

    Update search page

--- lib/pages/home/search_page.dart
+++ lib/pages/home/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:healthconnect/widgets/home_appbar.dart';
 import 'package:healthconnect/widgets/my_background.dart';
 
@@ -13,7 +14,59 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return MyBackground(
-        child: Scaffold(
-            appBar: homeAppBar(context), body: Container(color: Colors.green),),);
+      child: Scaffold(
+        appBar: homeAppBar(context),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [searchTextField()]),
+      ),
+    );
+  }
+
+  Widget searchTextField() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+          // focusedBorder: OutlineInputBorder(
+          //     borderRadius: BorderRadius.circular(12),
+          //     borderSide: BorderSide(
+          //         color: Theme.of(context).dividerColor.withOpacity(0.),
+          //         width: 0.6)),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+                color: Theme.of(context).dividerColor.withOpacity(0.2),
+                width: 0.6),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+                color: Theme.of(context).dividerColor,
+                width: 0.6),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+                color: Theme.of(context).dividerColor.withOpacity(0.4),
+                width: 0.6),
+          ),
+          hintStyle: GoogleFonts.roboto(
+            fontSize: 12,
+            color: Theme.of(context).dividerColor.withOpacity(0.6),
+          ),
+          hintText: 'Search',
+          contentPadding:
+              const EdgeInsets.only(top: 5, bottom: 5, right: 10, left: 16),
+          suffixIcon: Icon(
+            Icons.search,
+            size: 22,
+            color: Theme.of(context).dividerColor.withOpacity(0.6),
+          ),
+        ),
+      ),
+    );
   }
 }

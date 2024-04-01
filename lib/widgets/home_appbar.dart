54199bb5f8db3ab74570bc6275666024d635cca7
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-04-01 13:18:04 +0300

    Add gesture detector to profile page

--- lib/widgets/home_appbar.dart
+++ lib/widgets/home_appbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:healthconnect/constants/assets_constants.dart';
 import 'package:healthconnect/identifications/secret_ids.dart';
+import 'package:healthconnect/pages/home/profile_page.dart';
 
 AppBar homeAppBar(BuildContext context) {
   return AppBar(
@@ -15,77 +16,78 @@ AppBar homeAppBar(BuildContext context) {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 32,
-                width: 32,
-                child: Image.asset(AssetConstants.appLogo),
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 32,
+              width: 32,
+              child: Image.asset(AssetConstants.appLogo),
+            ),
+            const SizedBox(width: 8),
+            Text(
+              'HealthConnect',
+              style: GoogleFonts.roboto(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).dividerColor.withOpacity(0.9),
               ),
-              const SizedBox(width: 8),
+            ),
+          ],
+        ),
+        GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProfilePage(),
+              ),
+            );
+          },
+          child: SizedBox(
+            height: 40,
+            child: Row(children: [
               Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children:[
-                  Text('Health',
-                  style: GoogleFonts.roboto(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).dividerColor.withOpacity(0.7),),),
-                      const SizedBox(height: 2),
-                      Text('Connect',
-                  style: GoogleFonts.roboto(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).dividerColor.withOpacity(0.7),),),
-
-                ]
-              )
-              
-            ]),
-        SizedBox(
-          height: 40,
-          child: Row(children: [
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    UserData.userPoints,
-                    style: GoogleFonts.roboto(
-                      fontSize: 11,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).dividerColor.withOpacity(0.7),
-                    ),
-                  ),
-                  Text(UserData.userName,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      UserData.userPoints,
                       style: GoogleFonts.roboto(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).dividerColor.withOpacity(0.8),
-                      )),
-                ]),
-            const SizedBox(width: 6),
-            Column(
-              children: [
-                Container(
-                  height: 32,
-                  width: 32,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    image: const DecorationImage(
-                      image: AssetImage(UserData.userPic),
-                      fit: BoxFit.contain,
+                        fontSize: 11,
+                        fontWeight: FontWeight.w700,
+                        color: Theme.of(context).dividerColor.withOpacity(0.7),
+                      ),
+                    ),
+                    Text(UserData.userName,
+                        style: GoogleFonts.roboto(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color:
+                              Theme.of(context).dividerColor.withOpacity(0.8),
+                        )),
+                  ]),
+              const SizedBox(width: 6),
+              Column(
+                children: [
+                  Container(
+                    height: 32,
+                    width: 32,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      image: const DecorationImage(
+                        image: AssetImage(UserData.userPic),
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            )
-          ]),
-        )
+                ],
+              )
+            ]),
+          ),
+        ),
       ],
     ),
   );

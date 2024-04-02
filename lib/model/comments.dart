cfbf6a12e583132bfc518b12f2b8576b141535d4
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-04-02 20:08:58 +0300

    Comment box - add

---
+++ lib/model/comments.dart
@@ -0,0 +1,15 @@
+
+
+class Comment {
+  final String comment;
+  final String points;
+  final String user;
+  final String time;
+ 
+ Comment({
+    required this.comment,
+    required this.points,
+    required this.user,
+    required this.time,
+  });
+}
